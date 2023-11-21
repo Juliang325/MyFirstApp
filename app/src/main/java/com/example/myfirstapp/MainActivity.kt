044c54d737adc6b7694da5399d148fb04e24e25b
Juliang325 <133649655+Juliang325@users.noreply.github.com>
2023-11-21 23:39:23 +0100

    Toolbar y menu

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.myfirstapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 import com.example.myfirstapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,7 +17,11 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initListeners()
+        initToolbar()
+
     }
+
+    /***** LISTENERS ******/
 
     private fun initListeners() {
         binding.btnSaludar.setOnClickListener { checkValue() }
@@ -28,5 +35,45 @@ class MainActivity : AppCompatActivity() {
             intent.putExtra("name", binding.etName.text.toString())
             startActivity(intent)
         }
+    }
+
+    /***** TOOLBAR ******/
+
+    private fun initToolbar() {
+        var toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        moodToolbar(toolbar)
+    }
+
+    private fun moodToolbar(toolbar: Toolbar) {
+        toolbar.subtitle = "Es mi primera app"
+    }
+
+    /***** MENU ******/
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu,menu)
+        return true
+    }
+
+    public override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.item_info -> mostrarInfo()
+            else -> {
+                mostrarError()
+            }
+        }
+        return true
+    }
+
+
+
+    private fun mostrarInfo() {
+        Toast.makeText(this, "Esto es una app para principiantes", Toast.LENGTH_SHORT).show()
+    }
+
+    /***** MENSAJE DE ERROR ******/
+    private fun mostrarError() {
+        Toast.makeText(this, "Ha ocurrido un error", Toast.LENGTH_SHORT).show()
     }
 }
